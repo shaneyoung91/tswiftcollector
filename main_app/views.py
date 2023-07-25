c6e1956a66f298c87bfe3a0faf9fcb6120e159e0
shaneyoung91 <shaneadam.young@gmail.com>
2023-07-25 16:08:25 -0700

    Part 1 complete. Add HTML templates, CSS styling, About Page, All Taylors Page

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
-tswift = [
-    {'name': 'Debut Taylor', 'release-year': 2006, 'popular-songs': ['Teardrops on My Guitar', 'Tim McGraw', 'Our Song']},
-    {'name': 'Fearless Taylor', 'release-year': 2008, 'popular-songs': ['Love Story', 'You Belong With Me']},
-    {'name': 'Speak Now Taylor', 'release-year': 2010, 'popular-songs': ['Mean', 'Dear John', 'Enchanted', 'Back to December']},
-    {'name': 'Red Taylor', 'release-year': 2012, 'popular-songs': ['All Too Well', '22', 'I Knew You Were Trouble']},
-    {'name': '1989 Taylor', 'release-year': 2014, 'popular-songs': ['Blank Space', 'Bad Blood', 'Style', 'Wildest Dreams']},
-    {'name': 'Reputation Taylor', 'release-year': 2017, 'popular-songs': ['Delicate', 'Getaway Car', 'Gorgeous', '...Ready For It']},
-    {'name': 'Lover Taylor', 'release-year': 2019, 'popular-songs': ['Cruel Summer', 'Lover', 'Miss Americana & the Heartbreak Prince', 'London Boy']},
-    {'name': 'Midnights Taylor', 'release-year': 2022, 'popular-songs': ['Karma', 'Anti-Hero', 'Paris', 'Maroon']},
-]
+def home(request):
+    return render(request, 'home.html')
 
+def about(request):
+    return render(request, 'about.html')
+
+def taylors_index(request):
+    return render(request, 'taylors/index.html', {'taylors': taylors})
+
+taylors = [
+    {'name': 'Debut Taylor', 'year': 2006, 'songs': ['Teardrops on My Guitar', 'Tim McGraw', 'Our Song']},
+    {'name': 'Fearless Taylor', 'year': 2008, 'songs': ['Love Story', 'You Belong With Me']},
+    {'name': 'Speak Now Taylor', 'year': 2010, 'songs': ['Mean', 'Dear John', 'Enchanted', 'Back to December']},
+    {'name': 'Red Taylor', 'year': 2012, 'songs': ['All Too Well', '22', 'I Knew You Were Trouble']},
+    {'name': '1989 Taylor', 'year': 2014, 'songs': ['Blank Space', 'Bad Blood', 'Style', 'Wildest Dreams']},
+    {'name': 'Reputation Taylor', 'year': 2017, 'songs': ['Delicate', 'Getaway Car', 'Gorgeous', '...Ready For It']},
+    {'name': 'Lover Taylor', 'year': 2019, 'songs': ['Cruel Summer', 'Lover', 'Miss Americana & the Heartbreak Prince', 'London Boy']},
+    {'name': 'Midnights Taylor', 'year': 2022, 'songs': ['Karma', 'Anti-Hero', 'Paris', 'Maroon']},
+]
