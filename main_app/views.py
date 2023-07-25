6158fdbf731fc1c679158ca9809a54b0c20d2d32
shaneyoung91 <shaneadam.young@gmail.com>
2023-07-25 15:41:31 -0700

    Create list of tswift dictionaries. Implemented main_app

---
+++ main_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+tswift = [
+    {'name': 'Debut Taylor', 'release-year': 2006, 'popular-songs': ['Teardrops on My Guitar', 'Tim McGraw', 'Our Song']},
+    {'name': 'Fearless Taylor', 'release-year': 2008, 'popular-songs': ['Love Story', 'You Belong With Me']},
+    {'name': 'Speak Now Taylor', 'release-year': 2010, 'popular-songs': ['Mean', 'Dear John', 'Enchanted', 'Back to December']},
+    {'name': 'Red Taylor', 'release-year': 2012, 'popular-songs': ['All Too Well', '22', 'I Knew You Were Trouble']},
+    {'name': '1989 Taylor', 'release-year': 2014, 'popular-songs': ['Blank Space', 'Bad Blood', 'Style', 'Wildest Dreams']},
+    {'name': 'Reputation Taylor', 'release-year': 2017, 'popular-songs': ['Delicate', 'Getaway Car', 'Gorgeous', '...Ready For It']},
+    {'name': 'Lover Taylor', 'release-year': 2019, 'popular-songs': ['Cruel Summer', 'Lover', 'Miss Americana & the Heartbreak Prince', 'London Boy']},
+    {'name': 'Midnights Taylor', 'release-year': 2022, 'popular-songs': ['Karma', 'Anti-Hero', 'Paris', 'Maroon']},
+]
+
